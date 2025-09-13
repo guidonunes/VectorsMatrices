fb827b5354b5f5f6884307cfd3a68bb09add07a6
Guilherme Nunes <guilherme.augd@gmail.com>
2025-09-13 09:38:44 -0300

    add binary search example

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,5 +1,30 @@
 public class BinarySearch {
     public static void main(String[] args) {
+        int[] orderedVector = {11,22,33,44,55,66,77,88,99};
+        int searchedNumber = 55;
+        int start = 0;
+        int end = orderedVector.length - 1;
+        int foundAtIndex = -1;
+
+        while(start <= end) {
+            int mean = start + (end - start)/2;
+
+            if(orderedVector[mean] == searchedNumber) {
+                foundAtIndex = mean;
+                break;
+            } else if (orderedVector[mean] < searchedNumber) {
+                start = mean + 1;
+            } else {
+                end = mean - 1;
+            }
+
+        }
+
+        if(foundAtIndex != -1) {
+            System.out.println("Number found at index: " + foundAtIndex);
+        } else {
+            System.out.println("Number not found");
+        }
 
     }
 }
